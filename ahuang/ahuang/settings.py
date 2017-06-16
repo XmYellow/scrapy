baa8fb9f36f2f6372942f952f907333e2f86fa0d
xmyellow <286451004@qq.com>
2017-06-16 13:38:04 +0800

    delete pyc

--- ahuang/ahuang/settings.py
+++ ahuang/ahuang/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'ahuang.spiders'
 #USER_AGENT = 'ahuang (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -88,4 +88,4 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
-LOG_LEVEL=logging.getLevelName(logging.INFO)
+LOG_LEVEL=logging.getLevelName(logging.DEBUG)
